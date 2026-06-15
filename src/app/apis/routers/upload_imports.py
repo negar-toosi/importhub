47c7874ab3c7baa0c9195718734842b737e047c0
Negar Toosi <negar.tusi@gmail.com>
2026-06-15 19:21:27 +0330

    feat: create api to import excel and dto

--- src/app/apis/routers/upload_imports.py
+++ src/app/apis/routers/upload_imports.py
@@ -1,18 +1,30 @@
+import os
+import tempfile
+
 from fastapi import APIRouter, Depends
 
 from src.app.apis.dto.upload_imports import UploadImportRequest, UploadImportResponse
-from src.app.models import Import
-from src.app.utils.enums import ImportStatus
-from src.core.database import SessionDep
+from src.app.process_import import process_file
 from src.app.services import ImportServices
+from src.core.database import SessionDep
+
 router = APIRouter(prefix="/api/v1", tags=["imports"])
+
+UPLOAD_DIR = "/tmp/importhub_uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
 @router.post("/imports", response_model=UploadImportResponse, status_code=201)
 async def upload_import(session: SessionDep, request: UploadImportRequest = Depends()):
-    
+    file_suffix = os.path.splitext(request.file.filename)[1]
+    with tempfile.NamedTemporaryFile(delete=False, suffix=file_suffix, dir=UPLOAD_DIR) as tmp:
+        tmp.write(await request.file.read())
+        file_path = tmp.name
+
     record = ImportServices.create_import(session=session)
-    print("RECORD TYPE:", type(record))
+
+    process_file.delay(file_path, str(record.id))
+
     return UploadImportResponse(
         import_id=record.id,
         status=record.status,
